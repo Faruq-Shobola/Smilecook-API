5daff23db9462816f9ba95508b8c59e2e331424f
shobola faruq <shobolafaruq@gmail.com>
2023-04-04 12:38:57 +0100

    Get user details

--- resources/user.py
+++ resources/user.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 
 from utils import hash_password
 from models.user import User
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class UserListResource(Resource):
@@ -36,3 +37,28 @@ class UserListResource(Resource):
         }
 
         return data, HTTPStatus.CREATED
+
+
+class UserResource(Resource):
+    @jwt_required(optional=True)
+    def get(self, username):
+        user = User.get_by_username(username=username)
+
+        if user is None:
+            return {'message': 'user not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user == user.id:
+            data = {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            }
+        else:
+            data = {
+                'id': user.id,
+                'username': user.username
+            }
+
+        return data, HTTPStatus.OK
