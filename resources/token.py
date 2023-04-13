8d5bfd861d7113d114e1dd45394fe5e9e39ee6e6
shobola faruq <shobolafaruq@gmail.com>
2023-04-13 12:14:46 +0100

    Added refresh token functionality

--- resources/token.py
+++ resources/token.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 
 from utils import check_password
 from models.user import User
@@ -9,16 +9,27 @@ from models.user import User
 
 class TokenResource(Resource):
     def post(self):
-        json_data = request.get_json()
+        data = request.get_json()
 
-        email = json_data.get('email')
-        password = json_data.get('password')
+        email = data.get('email')
+        password = data.get('password')
 
         user = User.get_by_email(email=email)
 
         if not user or not check_password(password, user.password):
             return {'message': 'email or password is incorrect'}, HTTPStatus.UNAUTHORIZED
 
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
 
-        return {'access_token': access_token}, HTTPStatus.OK
+        return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
+
+
+class RefreshResource(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+
+        access_token = create_access_token(identity=current_user, fresh=False)
+
+        return {access_token: access_token}, HTTPStatus.OK
